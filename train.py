6cebfcabfb902fa59adf18dbab31c7204137386d
nyker510 <takamail53@gmail.com>
2019-04-29 18:25:28 +0900

    [update] logging など定義した関数を使うように修正

--- train.py
+++ train.py
@@ -1,23 +1,19 @@
 from __future__ import print_function
-import os
-from data import Dataset
-import torch
-from torch.utils import data
-import torch.nn.functional as F
-from models import *
-import torchvision
-from utils import Visualizer, view_model
-import torch
-import numpy as np
-import random
-import time
-from config import Config
-from torch.nn import DataParallel
+
 from torch.optim.lr_scheduler import StepLR
+from torch.utils import data
+
+from data.dataset import CASIADataset
 from test import *
+from utils import Visualizer
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 def save_model(model, save_path, name, iter_cnt):
+    os.makedirs(save_path, exist_ok=True)
+
     save_name = os.path.join(save_path, name + '_' + str(iter_cnt) + '.pth')
     torch.save(model.state_dict(), save_name)
     return save_name
@@ -30,7 +26,7 @@ if __name__ == '__main__':
         visualizer = Visualizer()
     device = torch.device("cuda")
 
-    train_dataset = Dataset(opt.train_root, opt.train_list, phase='train', input_shape=opt.input_shape)
+    train_dataset = CASIADataset(phase='train', input_shape=opt.input_shape)
     trainloader = data.DataLoader(train_dataset,
                                   batch_size=opt.train_batch_size,
                                   shuffle=True,
@@ -46,12 +42,7 @@ if __name__ == '__main__':
     else:
         criterion = torch.nn.CrossEntropyLoss()
 
-    if opt.backbone == 'resnet18':
-        model = resnet_face18(use_se=opt.use_se)
-    elif opt.backbone == 'resnet34':
-        model = resnet34()
-    elif opt.backbone == 'resnet50':
-        model = resnet50()
+    model = get_model(opt.backbone)
 
     if opt.metric == 'add_margin':
         metric_fc = AddMarginProduct(512, opt.num_classes, s=30, m=0.35)
@@ -71,18 +62,17 @@ if __name__ == '__main__':
 
     if opt.optimizer == 'sgd':
         optimizer = torch.optim.SGD([{'params': model.parameters()}, {'params': metric_fc.parameters()}],
-                                    lr=opt.lr, weight_decay=opt.weight_decay)
+                                    lr=opt.lr, weight_decay=opt.weight_decay, momentum=0.8, nesterov=True)
     else:
         optimizer = torch.optim.Adam([{'params': model.parameters()}, {'params': metric_fc.parameters()}],
                                      lr=opt.lr, weight_decay=opt.weight_decay)
     scheduler = StepLR(optimizer, step_size=opt.lr_step, gamma=0.1)
 
     start = time.time()
-    for i in range(opt.max_epoch):
+    for epoch in range(opt.max_epoch):
         scheduler.step()
-
         model.train()
-        for ii, data in enumerate(trainloader):
+        for i, data in enumerate(trainloader):
             data_input, label = data
             data_input = data_input.to(device)
             label = label.to(device).long()
@@ -93,26 +83,26 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
 
-            iters = i * len(trainloader) + ii
+            iters = epoch * len(trainloader) + i
 
             if iters % opt.print_freq == 0:
                 output = output.data.cpu().numpy()
                 output = np.argmax(output, axis=1)
                 label = label.data.cpu().numpy()
-                # print(output)
-                # print(label)
                 acc = np.mean((output == label).astype(int))
                 speed = opt.print_freq / (time.time() - start)
                 time_str = time.asctime(time.localtime(time.time()))
-                print('{} train epoch {} iter {} {} iters/s loss {} acc {}'.format(time_str, i, ii, speed, loss.item(), acc))
+                loss_val = loss.item()
+                logger.info(f'epoch: {epoch} iter {i} loss: {loss_val:.3f} acc :{acc:.3f} [iters/s {speed:.2f}]')
+
                 if opt.display:
                     visualizer.display_current_results(iters, loss.item(), name='train_loss')
                     visualizer.display_current_results(iters, acc, name='train_acc')
 
                 start = time.time()
 
-        if i % opt.save_interval == 0 or i == opt.max_epoch:
-            save_model(model, opt.checkpoints_path, opt.backbone, i)
+        if epoch % opt.save_interval == 0 or epoch == opt.max_epoch:
+            save_model(model, opt.checkpoints_path, opt.backbone, epoch)
 
         model.eval()
         acc = lfw_test(model, img_paths, identity_list, opt.lfw_test_list, opt.test_batch_size)
