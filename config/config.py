dea59a13f0cbf8e48f5fdfe43e9f898db9c79c50
nyker510 <takamail53@gmail.com>
2019-04-29 18:24:03 +0900

    [update] configuration compose
    dataset dir の値をつかってタイポなどしょうもないミスを防ぐことが目的

--- config/config.py
+++ config/config.py
@@ -1,4 +1,11 @@
+import os
+
+
 class Config(object):
+    """
+    学習/検証を実行する際の設定値
+    """
+
     env = 'default'
     backbone = 'resnet18'
     classify = 'softmax'
@@ -9,25 +16,32 @@ class Config(object):
     loss = 'focal_loss'
 
     display = False
+
     finetune = False
 
-    train_root = '/data/Datasets/webface/CASIA-maxpy-clean-crop-144/'
-    train_list = '/data/Datasets/webface/train_data_13938.txt'
+    DATASET_DIR = '/workdir/dataset/'
+
+    # CASIA DATASET のルートディレクトリ
+    CASIA_ROOT = os.path.join(DATASET_DIR, 'CASIA-WebFace')
+
     val_list = '/data/Datasets/webface/val_data_13938.txt'
 
     test_root = '/data1/Datasets/anti-spoofing/test/data_align_256'
     test_list = 'test.txt'
 
-    lfw_root = '/data/Datasets/lfw/lfw-align-128'
-    lfw_test_list = '/data/Datasets/lfw/lfw_test_pair.txt'
+    lfw_root = os.path.join(DATASET_DIR, 'lfw')
+    lfw_test_list = os.path.join(lfw_root, 'lfw_test_pair.txt')
 
-    checkpoints_path = 'checkpoints'
+    checkpoints_path = os.path.join(DATASET_DIR, 'checkpoints')
     load_model_path = 'models/resnet18.pth'
     test_model_path = 'checkpoints/resnet18_110.pth'
+
+    # 学習に関するパラメータ
+    # [TODO] arg で制御したい気分
     save_interval = 10
 
-    train_batch_size = 16  # batch size
-    test_batch_size = 60
+    train_batch_size = 100  # batch size
+    test_batch_size = 64
 
     input_shape = (1, 128, 128)
 
@@ -36,13 +50,13 @@ class Config(object):
     use_gpu = True  # use GPU or not
     gpu_id = '0, 1'
     num_workers = 4  # how many workers for loading data
-    print_freq = 100  # print info every N batch
+    print_freq = 1000  # print info every N batch
 
     debug_file = '/tmp/debug'  # if os.path.exists(debug_file): enter ipdb
     result_file = 'result.csv'
 
     max_epoch = 50
-    lr = 1e-1  # initial learning rate
+    lr = .05  # initial learning rate
     lr_step = 10
     lr_decay = 0.95  # when val_loss increase, lr = lr*lr_decay
-    weight_decay = 5e-4
+    weight_decay = 0
