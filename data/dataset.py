31edc6cc0eabd7a41d2ec055f1dc4725530e4483
nyker510 <takamail53@gmail.com>
2019-05-03 12:51:52 +0900

    [update] データの作成コードを実装

--- data/dataset.py
+++ data/dataset.py
@@ -1,26 +1,31 @@
 import os
+from glob import glob
+from typing import List
 
 import pandas as pd
 from PIL import Image
 from torch.utils import data
 from torchvision import transforms as T
+
 import env
 
 
 class AbstractDataset(data.Dataset):
     root_path = None
+    meta_filename = 'meta.csv'
     relative_path = True
     n_classes = None
 
+    label_colname = 'label'
+    img_colname = 'img_path'
+
     def __init__(self,
                  phase='train',
-                 input_shape=(1, 128, 128)):
+                 input_shape=(1, 128, 128),
+                 recreate=False):
 
         self.phase = phase
         self.input_shape = input_shape
-        self.df_meta = pd.read_csv(os.path.join(self.root_path, 'meta.csv'))
-        self.index_to_data = self.df_meta.to_dict(orient='index')
-
         normalize = T.Normalize(mean=[0.5], std=[0.5])
         if self.phase == 'train':
             self.transforms = T.Compose([
@@ -36,6 +41,9 @@ class AbstractDataset(data.Dataset):
                 normalize
             ])
 
+        self.df_meta = self.read_metadata(force=recreate)
+        self.index_to_data = self.df_meta.to_dict(orient='index')
+
     def __str__(self):
         cls_name = self.__class__.__name__
         s = cls_name.replace('Dataset', '')
@@ -50,6 +58,14 @@ class AbstractDataset(data.Dataset):
         return s
 
     @property
+    def meta_path(self):
+        return os.path.join(self.root_path, self.meta_filename)
+
+    @property
+    def exist_metadata(self):
+        return os.path.exists(self.meta_path)
+
+    @property
     def is_greyscale(self):
         return self.input_shape[0] == 1
 
@@ -60,9 +76,21 @@ class AbstractDataset(data.Dataset):
         else:
             return 'RGB'
 
+    def read_metadata(self, force=False):
+        if self.exist_metadata and not force:
+            return pd.read_csv(self.meta_path)
+
+        print('create metadata')
+        df = self.create_metadata()
+        df.to_csv(self.meta_path, index=False)
+        return df
+
+    def create_metadata(self) -> pd.DataFrame:
+        raise NotImplementedError()
+
     def __getitem__(self, index):
         data = self.index_to_data[index]
-        img_path, label = data['img_path'], data['label']
+        img_path, label = data[self.img_colname], data[self.label_colname]
 
         if self.relative_path:
             img_path = os.path.join(self.root_path, img_path)
@@ -75,10 +103,35 @@ class AbstractDataset(data.Dataset):
         return len(self.df_meta)
 
 
-class CASIADataset(AbstractDataset):
+class CASIAFullDataset(AbstractDataset):
     root_path = os.path.join(env.DATASET_DIR, 'CASIA-WebFace')
     relative_path = False
-    n_classes = 912
+    meta_filename = 'meta_full.csv'
+    min_value_count = 20
+
+    def create_metadata(self):
+        img_paths = glob(os.path.join(self.root_path, '*/*.jpg'))
+        df_meta = pd.DataFrame(img_paths, columns=['img_path'])
+        df_meta['dir_name'] = [str(p.split('/')[-2]) for p in img_paths]
+        vc = df_meta.dir_name.value_counts()
+        use_dirnames = self.get_use_dirnames(vc)
+        df_label = pd.DataFrame(use_dirnames, columns=['dir_name'])
+        df_label.index.name = 'label'
+        df_label = df_label.reset_index()
+        df_meta = pd.merge(df_meta, df_label, on='dir_name', how='right')
+        return df_meta
+
+    def get_use_dirnames(self, vc) -> List[str]:
+        use = vc[vc >= self.min_value_count]
+        return use.index
+
+    @property
+    def n_classes(self):
+        return len(self.df_meta[self.label_colname].unique())
+
+
+class CASIADataset(CASIAFullDataset):
+    min_value_count = 200
 
 
 class CelebaDataset(AbstractDataset):
@@ -89,6 +142,7 @@ class CelebaDataset(AbstractDataset):
 
 def get_dataset(name, *args, **kwargs) -> AbstractDataset:
     all_datasets = [
+        CASIAFullDataset,
         CASIADataset,
         CelebaDataset
     ]
